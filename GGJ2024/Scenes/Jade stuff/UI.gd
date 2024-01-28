83b2d903b6f5d2f05f12d46e6298f37b68de0d73
Jade Spooner <jade.spoon2003@gmail.com>
2024-01-28 12:25:36 -0500

    UI and rigging skeleton

---
+++ GGJ2024/Scenes/Jade stuff/UI.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@export var blinkTime = 1.5
+@export var scrollTime = 3
+@onready var liveSign := $LiveSign
+
+var curTime
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
