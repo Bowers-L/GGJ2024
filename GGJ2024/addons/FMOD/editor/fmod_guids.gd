9849502dcb2f71a5791d09b034c0375b735d862f
reverienest <44439363+reverienest@users.noreply.github.com>
2024-01-28 13:13:38 -0500

    Updated FMOD Banks and Added StudioBankLoader

---
+++ GGJ2024/addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,22 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'GGJ2024-FMOD.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const MUSIC_NEWSMUSIC = "{b7a2ff2e-23b8-4e65-8455-d31090fd5679}"
+	const MUSIC_THE_BUILDER = "{0e04b72c-1f3b-489a-837b-26551384d629}"
+	const SFX_DESTRUCTION = "{c9a00d40-45e9-4cd7-956a-e1d6805febb6}"
+	const SFX_FOOTSTEP = "{a8465e31-5387-41d5-bc9a-fd4e00ea6514}"
+	const SFX_KLUNKSTER = "{867a6197-331f-4e40-a6c5-b9951f6eae7f}"
+
+class Busses:
+	const MASTER_BUS = "{403f67cc-8c0b-4f6a-b588-b642f45fd1c5}"
+	const REVERB = "{80de0701-5629-45f1-a179-8975d76eb70d}"
+
+class Banks:
+	const MASTER = "{bcaf0aa4-49ec-4aa6-b6a0-ddd5dbb350ea}"
+
