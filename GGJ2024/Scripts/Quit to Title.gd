0380f02a339c357ac73f68c4aeb17bc2bcf3c910
Mitchell Philipp <mgpalpha1@gmail.com>
2024-02-07 22:02:15 -0500

    full controller support

--- GGJ2024/Scripts/Quit to Title.gd
+++ GGJ2024/Scripts/Quit to Title.gd
@@ -5,14 +5,14 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://Scenes/Jade stuff/Title Screen.tscn")
+#func _unhandled_input(event):
+	#if event is InputEventKey:
+		#if event.pressed and event.keycode == KEY_ESCAPE:
+			#get_tree().change_scene_to_file("res://Scenes/Jade stuff/Title Screen.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	#print("trying")
-	#if Input.is_action_just_pressed("ui_cancel"):
-		#print("pressed")
+	if Input.is_action_just_pressed("ui_cancel"):
+		print("trying quit")
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/Jade stuff/Title Screen.tscn")
